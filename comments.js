339d8da7fa83b0a268c69e1abf75531d2f0530d9
zeeed2022 <121613195+zeeed2022@users.noreply.github.com>
2023-07-19 06:04:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,16 @@
-// create web server to handle comments
-// Date: 3/19/2019
+// create web server
+// 1. import http
+const http = require('http');
+// 2. create web server
+const server = http.createServer((req, res) => {
+    // 3.1 set header
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    // 3.2 send response
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+// 3. listen to incoming requests
+server.listen(3000);
