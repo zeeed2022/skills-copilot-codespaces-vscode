dfa0dd6be67a9b000827f7bc88af49dd38276693
zeeed2022 <121613195+zeeed2022@users.noreply.github.com>
2023-07-19 05:17:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+  var about = document.getElementById("about");
+  var member = document.getElementById("member");
+
+  member.style.display = "block";
+  skills.style.display = "none";
+  projects.style.display = "none";
+  contact.style.display = "none";
+  about.style.display = "none";
+  member.style.display = "block";
+}   
